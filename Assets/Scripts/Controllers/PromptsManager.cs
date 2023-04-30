a937bede24f7ccec2302e07963a4ba591f37f8f5
PijusVose <pijus.vose@gmail.com>
2023-04-30 13:16:31 +0300

    added interactions collider to player; added Singleton base class;

--- Assets/Scripts/Controllers/PromptsManager.cs
+++ Assets/Scripts/Controllers/PromptsManager.cs
@@ -1,10 +1,22 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class PromptsManager : MonoBehaviour
 {
-    [SerializeField] private List<IPrompt> prompts;
+    [SerializeField] private GameObject worldObjectsParent;
+
+    private List<IInteractable> interactables;
+    private List<IPrompt> prompts;
+
+    private void Start()
+    {
+        if (worldObjectsParent != null)
+            interactables = worldObjectsParent.GetComponentsInChildren<IInteractable>().ToList();
+    }
+    
     
     // TODO: on update, hide or show prompts depending on distance.
 }
